b7eb29cc2172cf137844897c2c2f16c13bc6f0c6
Harper Frankstone <Harper Frankstone>
2017-01-31 20:22:11 -0600

    chasing the ethereal console log

--- strings.js
+++ strings.js
@@ -1,26 +1,31 @@
 "use strict";
 
 console.log("Here we go...");
-var inputType = document.getElementsByClassName("userString");
+var inputType = document.getElementsByClassName("userString").value;
+
 var buttonPress = document.getElementsByClassName("activate");
+
 
 //function to run the string functions upon 'enter' keypress
 function enterPress(event) {
     if (event.keyCode === 13) {
-    	var testString = "";
-		reversal(testString);
-		palindrome(testString);
-		alphabits(testString);
+    	var testString = inputType;
+		// reversal(testString);
+		// alphabits(testString);
+		// palindrome(testString);
     } 
+	// console.log(testString);
 };
 
 //function to run the string functions on the button press
 
 function onClick(){
-	var testString = "";
-	reversal(testString);
-	palindrome(testString);
-	alphabits(testString);
+	var testString = inputType;
+	// reversal(inputType);
+	// alphabits(testString);
+	// palindrome(testString);
+	console.log("The input so far is.. ", testString);
+
 };
 
 //function to determine if the user's input is a valid character, shows an alert if it's not
@@ -36,47 +41,47 @@ function onClick(){
 //STRING FUNCTIONS
 
 
-function reversal(str) {
-	//spliting the str up
-	var stringSplit = str.split('');
+// function reversal(inputType) {
+// 	//spliting the str up
+// 	var stringSplit = inputType.split("");
 
-	//reversing the split string
-	var backwards = stringSplit.reverse();
+// 	//reversing the split string
+// 	var backwards = stringSplit.reverse();
 
-	//joining the reversed string
-	var backTogether = backwards.join('');
+// 	//joining the reversed string
+// 	var backTogether = backwards.join(" ");
 
-	//give me the reversed string
-	console.log(backTogether);
-	return backTogether;
-}
-
-
-
-function alphabits(str) {
-	//split the string up 
-	var stringSplit = str.split('');
-
-	//sorting the split string
-	var alphabet = stringSplit.sort();
-
-	//joining the sorted string
-	var inOrder = alphabet.join('');
-
-	console.log(inOrder);
-	return inOrder;
-}
+// 	//give me the reversed string
+// 	console.log(backTogether);
+// 	return backTogether;
+//  }
 
 
 
-function palindrome(str) {
-	//split, reverse, then join the string
-	// var newWord = ;
-	if(str === str.split('').reverse().join('')){//this isn't working and I am not sure why
-		alert("That's a palindrome!")
-	} else {
-		alert("I'm not mad...just disappointed you didn't give me a palindrome.");
-	}
-}
+// // function alphabits(str) {
+// 	//split the string up 
+// 	var stringSplit = str.split('');
+
+// 	//sorting the split string
+// 	var alphabet = stringSplit.sort();
+
+// 	//joining the sorted string
+// 	var inOrder = alphabet.join('');
+
+// 	console.log(inOrder);
+// 	return inOrder;
+// }
+
+
+
+// // function palindrome(str) {
+// 	//split, reverse, then join the string
+// 	// var newWord = ;
+// 	if(str === str.split('').reverse().join('')){//this isn't working and I am not sure why
+// 		alert("That's a palindrome!")
+// 	} else {
+// 		alert("I'm not mad...just disappointed you didn't give me a palindrome.");
+// 	}
+// }
 
 
